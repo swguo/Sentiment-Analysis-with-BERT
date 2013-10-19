192eedb212e2f06a71b75ba6401608a6746f3b87
mokoversity-camp3 <cometlcc@gmail.com>
2013-10-19 16:51:37 +0800

    1019 final

--- main.js
+++ main.js
@@ -2,17 +2,32 @@
 var gameModule = (function(){
 var timeOutvar,
 	count = 0;
-	function start(){
+	function print(){
 
-		var canvas = document.getElementById('game'),
-		 ballx = Math.floor(Math.random()*300),
+		console.log('end');
+	}
+	function RGB2HTML(red, green, blue)
+	{
+	    var decColor = red + 256 * green + 65536 * blue;
+	    return "#"+decColor.toString(16);
+	}
+	canvas = document.getElementById('game');
+	canvas.width = 400;
+	canvas.height = 320;
+
+	function start(){
+		var r=Math.floor(Math.random()*255);
+		var g=Math.floor(Math.random()*255);
+		var b=Math.floor(Math.random()*255);
+		console.log(RGB2HTML(r,g,b));
+		var c=RGB2HTML(r,g,b);
+		var  ballx = Math.floor(Math.random()*300),
 		 bally = Math.floor(Math.random()*500),
 		 ballz = Math.floor(Math.random()*100);
  		ctx = canvas.getContext('2d');
 
-	  		canvas.width = 400;
-	  		canvas.height = 320;
-	  		ctx.fillStyle = 'red';
+	  		
+	  		ctx.fillStyle = c;
 	  		
 	  		ctx.beginPath();
 	  		ctx.arc(ballx,bally,ballz,0,Math.PI*2,true);
@@ -20,7 +35,7 @@ var timeOutvar,
 
 	  		if(count>=10){
 
-
+				print();
 
 		  	}else{
 
@@ -29,6 +44,7 @@ var timeOutvar,
 		  		count = count+1;
 		  		console.log("Counter: " + count);
 		  	}
+
 	  	
 	  	}
 
@@ -36,7 +52,7 @@ var timeOutvar,
 
 	  	return{
 
-
+	  		print: print,
 	  		start: start
 	  	}
 
@@ -44,13 +60,35 @@ var timeOutvar,
 
 gameModule.start();
 
-(function(){
-var c=document.getElementById("frant");
-var ctx=c.getContext("2d");
-ctx.fillStyle = 'yellow';
-ctx.font="30px Arial";
-ctx.fillText("Hello World",10,50);
-	  		
+
+
+
+
+
+
+
+
+
+var getfrontcolor=(function(){
+
+	function start(){
+
+	var c=document.getElementById("frant");
+	var ctx=c.getContext("2d");
+	ctx.fillStyle = 'rgb(0,0,0)';
+	ctx.font="30px Arial";
+	ctx.fillText("by 士煒",10,50);
+
+	timeOutvar = setTimeout(start,1000);
+
+	}
+	return{
+
+		start:start
+
+	}
 })();
+
+getfrontcolor.start();
 
 
