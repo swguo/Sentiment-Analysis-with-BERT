e9ccbbd310a143df8e7d0f44ac341ae5f93534e5
mokoversity-camp3 <cometlcc@gmail.com>
2013-10-19 15:08:12 +0800

    完成 module,fix #9

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
-(function(){
+var gameModule = (function(){
 
-var canvas = document.getElementById('game'),
- ballx = Math.floor(Math.random()*300),
- bally = Math.floor(Math.random()*500),
- ballz = Math.floor(Math.random()*100);
- ctx = canvas.getContext('2d');
+	function start(){
+
+		var canvas = document.getElementById('game'),
+		 ballx = Math.floor(Math.random()*300),
+		 bally = Math.floor(Math.random()*500),
+		 ballz = Math.floor(Math.random()*100);
+ 		ctx = canvas.getContext('2d');
 
 	  		canvas.width = 400;
 	  		canvas.height = 320;
@@ -14,8 +16,25 @@ var canvas = document.getElementById('game'),
 	  		ctx.beginPath();
 	  		ctx.arc(ballx,bally,ballz,0,Math.PI*2,true);
 	  		ctx.fill();
+	  	}
+
+	  	return{
+
+
+	  		start: start
+	  	}
 
 })();
+
+gameModule.start();
+
+(function(){
+var c=document.getElementById("frant");
+var ctx=c.getContext("2d");
+ctx.fillStyle = 'yellow';
+ctx.font="30px Arial";
+ctx.fillText("Hello World",10,50);
 	  		
+})();
 
 
