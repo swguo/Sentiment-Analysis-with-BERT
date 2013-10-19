670b6faf77fc2b7080aa521f842978ffa9e167a5
mokoversity-camp3 <cometlcc@gmail.com>
2013-10-19 14:43:30 +0800

    進階宣告方式

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 
 (function(){
 
-var canvas = document.getElementById('game');
-	  		var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('game'),
+ ballx = Math.floor(Math.random()*300),
+ bally = Math.floor(Math.random()*500),
+ ballz = Math.floor(Math.random()*100);
+ ctx = canvas.getContext('2d');
 
 	  		canvas.width = 400;
 	  		canvas.height = 320;
 	  		ctx.fillStyle = 'red';
-	  		var ballx = Math.floor(Math.random()*300);
-	  		var bally = Math.floor(Math.random()*500);
-	  		var ballz = Math.floor(Math.random()*100);
+	  		
 	  		ctx.beginPath();
 	  		ctx.arc(ballx,bally,ballz,0,Math.PI*2,true);
 	  		ctx.fill();
